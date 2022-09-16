938079ed5c0322a790788738df6aac847de338fb
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-09-16 10:57:01 +0300

    add some points to reset

--- src/js/blank6.js
+++ src/js/blank6.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    $('.reviews-cards-list').slick({
-        dots: true,
-        speed: 700,
-        infinite: true,
-        // autoplay: true,
-        // autoplaySpeed: 4000,
-        // pauseOnFocus: true,
-        // pauseOnHover: true,
-        // pauseOnDotsHover: true
-    });
+	$('.reviews-cards-list').slick({
+		dots: true,
+		speed: 700,
+		infinite: true,
+		// autoplay: true,
+		// autoplaySpeed: 4000,
+		// pauseOnFocus: true,
+		// pauseOnHover: true,
+		// pauseOnDotsHover: true
+	});
 });
 
 
