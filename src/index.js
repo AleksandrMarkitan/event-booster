667ebefcef6269eb6b1143a8a12fe5d9be2349af
Dafarlog <Dafarlog@gmail.com>
2022-09-17 00:36:31 +0300

    add getCurentPage getTotalPages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './js/blank1';
-import "./js/eventsAPI"
+import { eventsAPI } from './js/eventsAPI';
+
+
+// EventsAPI.getEvents({countryCode: "us", page: "3"});
+// EventsAPI.getEvents({id: "G5v0Z98m20vKU"});
--- src/js/eventsAPI.js
+++ src/js/eventsAPI.js
@@ -1,16 +1,45 @@
 import axios from "axios";
-
-async function getEvents(options={}){
-    const {id = "", countryCode = "", keyword =""}=options;
-
-    const res = await axios.get("https://app.ticketmaster.com/discovery/v2/events.json",
-    {
-        params:{
-            ...options,
-            apikey: "GfgVh4R7D4nbAHFUVOA6O71Si5aOA40O",
+export class EventsAPI{
+    static #apikey = "GfgVh4R7D4nbAHFUVOA6O71Si5aOA40O";
+    static #params ={};
+    static #totalPages = 0; 
+    static #curentPage = 0;
+    static async getEvents(options={}){
+        const {countryCode = "", keyword ="", size =""}=options;
+        if(keyword.trim()||countryCode.trim()){
+            this.#params = {
+                ...options, 
+            }
         }
-    })
-    console.log(res.data);
+        try{
+            const res = await axios.get("https://app.ticketmaster.com/discovery/v2/events.json",
+            {
+                params: {
+                    ...this.#params,
+                    apikey: this.#apikey,
+                }
+            })
+
+            await ({ totalPages: this.#totalPages, number: this.#curentPage } = res.data.page)
+            return res.data._embedded;
+        }catch(e){
+            console.log(e);
+        } 
+    }
+    static getCurentPage(){
+        return this.#curentPage;
+    }
+    static getTotalPages(){
+        return this.#totalPages;
+    } 
 }
-const options = {id: ""}
-getEvents(options)
+
+async function tmp(){
+    console.log(await EventsAPI.getEvents())
+    console.log(EventsAPI.getCurentPage());
+    console.log(EventsAPI.getTotalPages());
+}
+
+tmp();
+
+
