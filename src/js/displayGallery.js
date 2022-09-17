5efb4694edbae26f76e332df22e1f45a7b3daf24
Dafarlog <Dafarlog@gmail.com>
2022-09-17 18:39:20 +0300

    add vars

--- src/js/displayGallery.js
+++ src/js/displayGallery.js
@@ -2,13 +2,17 @@ import { EventsAPI } from "./eventsAPI";
 
 export async function displayGallery(options){
     const resp = await EventsAPI.getEvents(options);
-    galleryMarkup(await EventsAPI.getEvents(options));
-    console.log(1);
-    
+    galleryMarkup(await EventsAPI.getEvents(options));   
 }
 
 function galleryMarkup(arr = []){
-    arr.reduce((acc, elem)=>{return acc})
+    arr.reduce((acc, event)=>{
+        
+        const {name, id, images, dates:{start: {localDate}}, _embedded:{venues}} = event;
+        const {name: nameOfPlace} =venues[0]
+        console.log(nameOfPlace);
+
+        return acc += ``;})
     // ((acc, event)=>{
     //     const {name, id, images, dates:{start: {localDate}}, place} = event;
     //     console.log(place);s
