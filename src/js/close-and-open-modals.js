aec8552be1bfac1f482bb839616d58dbc5706224
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-09-17 18:52:06 +0300

    add escape and backdrop close

--- src/js/close-and-open-modals.js
+++ src/js/close-and-open-modals.js
@@ -1,22 +1,37 @@
 const openModalTeam = document.querySelector('.js-open-team-modal');
 const modalTeam = document.querySelector('[data-modal-team]');
 const closeTeam = document.querySelector('[data-close-team]');
+const backdropModalTeam = document.querySelector('[data-modal-team]')
 
 const modal = document.querySelector('[data-modal]');
 const closeModal = document.querySelector('[data-close]');
-const galleryCard = document.querySelector('.gallery__card')
+const galleryCard = document.querySelector('.js-gallery-card')
+const backdropModal = document.querySelector('[data-modal]');
 
 
 openModalTeam.addEventListener('click', addHiddenClassToTeam);
 closeTeam.addEventListener('click', addHiddenClassToTeam);
-
-function addHiddenClassToTeam() {
-	modalTeam.classList.toggle("visually-hidden");
-}
+backdropModalTeam.addEventListener('click', addHiddenClassToTeam);
 
 galleryCard.addEventListener('click', addHiddenClassToModal);
 closeModal.addEventListener('click', addHiddenClassToModal);
+backdropModal.addEventListener('click', addHiddenClassToModal)
 
-function addHiddenClassToModal() {
+function addHiddenClassToTeam() {
+	modalTeam.classList.toggle("visually-hidden");
+	closeByKeybord(modalTeam)
+}
+
+function addHiddenClassToModal(e) {
+	e.preventDefault();
 	modal.classList.toggle("visually-hidden");
+	closeByKeybord(modal)
+}
+
+function closeByKeybord(value) {
+	document.addEventListener("keydown", (e) => {
+		if (e.code == "Escape") {
+			value.classList.toggle("visually-hidden");
+		}
+	}, { once: true });
 }
