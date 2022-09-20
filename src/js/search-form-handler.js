932e2c03dfeb0bfbfc15e4df73f9e0b685c27eec
AleksandrMarkitan <0688138@gmail.com>
2022-09-20 17:46:27 +0300

    Merge branch fix placeholder

--- src/js/search-form-handler.js
+++ src/js/search-form-handler.js
@@ -6,22 +6,21 @@ import { findeCountry } from './select-country-markup';
 import svg from '../images/sprite.svg';
 
 const refs = {
-	selectCountryField: document.querySelector('.select-country'),
-	// selectCountryBtn: document.querySelector('.select-country-btn'),
-	galleryList: document.querySelector('.gallery__list'),
-	countrylistThumb: document.querySelector('.country_list-thumb'),
-	countrylist: document.querySelector('.country_list'),
-	keyword: document.querySelector('.keyword'),
-	searchSvg: document.querySelector('.search__svg'),
-	selectSvg: document.querySelector('.select__svg'),
+  keyword: document.querySelector('.keyword'),
+  selectCountryField: document.querySelector('.select-country'),
+  galleryList: document.querySelector('.gallery__list'),
+  countrylistThumb: document.querySelector('.country_list-thumb'),
+  countrylist: document.querySelector('.country_list'),
+  searchSvg: document.querySelector('.search__svg'),
+  selectSvg: document.querySelector('.select__svg'),
 };
 
 let countryCode = '';
 let keyword = '';
 
 refs.selectCountryField.addEventListener(
-	'input',
-	debounce(onSelectCountryFieldHdlr, 500)
+  'input',
+  debounce(onSelectCountryFieldHdlr, 500)
 );
 refs.selectCountryField.addEventListener('focus', onSelectCountryFocusHdlr);
 refs.selectCountryField.addEventListener('blur', onSelectCountryBlurHdlr);
@@ -31,30 +30,30 @@ refs.keyword.addEventListener('focus', onKeywordFocusHdlr);
 refs.keyword.addEventListener('blur', onKeywordBlurHdlr);
 
 function onSelectCountryFieldHdlr(e) {
-	const filterCountries = findeCountry(e.target.value, countries);
-	refs.countrylist.innerHTML = markapCountryList(filterCountries);
-	refs.selectCountryField.classList.add('select__field');
-	refs.countrylistThumb.classList.remove('is-hidden');
+  const filterCountries = findeCountry(e.target.value, countries);
+  refs.countrylist.innerHTML = markapCountryList(filterCountries);
+  refs.selectCountryField.classList.add('select__field');
+  refs.countrylistThumb.classList.remove('is-hidden');
 }
 
 function onSelectCountryFocusHdlr(e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	refs.countrylist.innerHTML = markapCountryList(countries);
-	refs.countrylistThumb.classList.toggle('is-hidden');
-	refs.selectCountryField.classList.toggle('select__field');
-	refs.selectCountryField.placeholder = 'Choose country';
-	refs.selectSvg.classList.add('focus');
+  refs.countrylist.innerHTML = markapCountryList(countries);
+  refs.countrylistThumb.classList.toggle('is-hidden');
+  refs.selectCountryField.classList.toggle('select__field');
+  refs.selectCountryField.placeholder = 'Choose country';
+  refs.selectSvg.classList.add('focus');
 }
 
 function onSelectCountryBlurHdlr(e) {
-	refs.countrylistThumb.classList.add('is-hidden');
-	refs.selectCountryField.classList.remove('select__field');
-	refs.selectSvg.classList.remove('focus');
+  refs.countrylistThumb.classList.add('is-hidden');
+  refs.selectCountryField.classList.remove('select__field');
+  refs.selectSvg.classList.remove('focus');
 }
 
 function onCountrylistHdlr(e) {
-	countryCode = e.target.dataset.id;
+  countryCode = e.target.dataset.id;
 
 	if (countryCode === 'RU') {
 		refs.galleryList.innerHTML =
@@ -76,31 +75,22 @@ function onCountrylistHdlr(e) {
 		  </a>
 	 </div></li>`
 
-		// 	  `<li class="ru"><h1>РУССКИЙ ВОЕННЫЙ КОРАБЛЬ!<br>ИДИ НАХУЙ!!!</h1><img
-		//   src="https://blackbee.odessa.ua/wp-content/uploads/2022/04/russkij-voennyj-korabl-idi-foto.jpg"
-		//   alt="РУССКИЙ ВОЕННЫЙ КОРАБЛЬ! ИДИ НАХУЙ!!!"
-		//   width="1200"
-		// /></li>`;
-
 		refs.selectCountryField.placeholder = 'Choose another country';
-	} else {
-		refs.selectCountryField.placeholder = e.target.textContent;
-		displayGallery({ countryCode, keyword });
-	}
-	refs.countrylistThumb.classList.add('is-hidden');
-	refs.selectCountryField.classList.remove('select__field');
-	refs.selectCountryField.value = '';
-}
+	} else if (e.target.nodeName === 'LI') {
+      refs.selectCountryField.placeholder = e.target.textContent;
+      displayGallery({ countryCode, keyword });
+    }
+  
 
 function onKeywordHdlr(e) {
-	keyword = e.target.value;
-	displayGallery({ countryCode, keyword });
+  keyword = e.target.value;
+  displayGallery({ countryCode, keyword });
 }
 
 function onKeywordFocusHdlr() {
-	refs.searchSvg.classList.add('focus');
+  refs.searchSvg.classList.add('focus');
 }
 
 function onKeywordBlurHdlr(e) {
-	refs.searchSvg.classList.remove('focus');
+  refs.searchSvg.classList.remove('focus');
 }
