00329a9eee93dd4cc71b9f245979c781127dc9bd
AleksandrMarkitan <0688138@gmail.com>
2022-09-20 03:23:33 +0300

    стилі поля пошуку країни

--- src/js/search-form-handler.js
+++ src/js/search-form-handler.js
@@ -6,11 +6,13 @@ import { findeCountry } from './select-country-markup';
 
 const refs = {
   selectCountryField: document.querySelector('.select-country'),
-  selectCountryBtn: document.querySelector('.select-country-btn'),
+  // selectCountryBtn: document.querySelector('.select-country-btn'),
   galleryList: document.querySelector('.gallery__list'),
   countrylistThumb: document.querySelector('.country_list-thumb'),
   countrylist: document.querySelector('.country_list'),
   keyword: document.querySelector('.keyword'),
+  searchSvg: document.querySelector('.search__svg'),
+  selectSvg: document.querySelector('.select__svg'),
 };
 
 let countryCode = '';
@@ -20,9 +22,12 @@ refs.selectCountryField.addEventListener(
   'input',
   debounce(onSelectCountryFieldHdlr, 500)
 );
-refs.selectCountryBtn.addEventListener('click', onSelectCountryBtnHdlr);
+refs.selectCountryField.addEventListener('focus', onSelectCountryFocusHdlr);
+refs.selectCountryField.addEventListener('blur', onSelectCountryBlurHdlr);
 refs.countrylist.addEventListener('click', onCountrylistHdlr);
 refs.keyword.addEventListener('input', debounce(onKeywordHdlr, 500));
+refs.keyword.addEventListener('focus', onKeywordFocusHdlr);
+refs.keyword.addEventListener('blur', onKeywordBlurHdlr);
 
 function onSelectCountryFieldHdlr(e) {
   const filterCountries = findeCountry(e.target.value, countries);
@@ -31,12 +36,20 @@ function onSelectCountryFieldHdlr(e) {
   refs.countrylistThumb.classList.remove('is-hidden');
 }
 
-function onSelectCountryBtnHdlr(e) {
+function onSelectCountryFocusHdlr(e) {
   e.preventDefault();
 
   refs.countrylist.innerHTML = markapCountryList(countries);
   refs.countrylistThumb.classList.toggle('is-hidden');
   refs.selectCountryField.classList.toggle('select__field');
+  refs.selectCountryField.placeholder = 'Choose country';
+  refs.selectSvg.classList.add('focus');
+}
+
+function onSelectCountryBlurHdlr(e) {
+  refs.countrylistThumb.classList.add('is-hidden');
+  refs.selectCountryField.classList.remove('select__field');
+  refs.selectSvg.classList.remove('focus');
 }
 
 function onCountrylistHdlr(e) {
@@ -61,4 +74,12 @@ function onCountrylistHdlr(e) {
 function onKeywordHdlr(e) {
   keyword = e.target.value;
   displayGallery({ countryCode, keyword });
+}
+
+function onKeywordFocusHdlr() {
+  refs.searchSvg.classList.add('focus');
+}
+
+function onKeywordBlurHdlr(e) {
+  refs.searchSvg.classList.remove('focus');
 }
